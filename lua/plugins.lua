5553d5ca94b8b0effa1e5a05bf138cfeec463ad8
Young, Gianni <gianni.young@nelnet.net>
2022-06-09 16:18:12 -0500

    I don't give a damn 'bout my reputation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,6 +14,29 @@ require('packer').startup(function(use)
     as = 'dracula'
   }
 
+  use {
+  'kdheepak/tabline.nvim',
+  config = function()
+    require'tabline'.setup {
+      enable = true,
+      options = {
+        max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+        show_tabs_always = true, -- this shows tabs only when there are more than one tab or if the first tab is named
+        show_devicons = true, -- this shows devicons in buffer section
+        show_bufnr = false, -- this appends [bufnr] to buffer section,
+        show_filename_only = true, -- shows base filename only instead of relative path in filename
+        modified_icon = "+ ", -- change the default modified icon
+        modified_italic = true, -- set to true by default; this determines whether the filename turns italic if modified
+        show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+      }
+    }
+    vim.cmd[[
+      set guioptions-=e " Use showtabline in gui vim
+      set sessionoptions+=tabpages,globals " store tabpages and globals in session
+    ]]
+  end,
+  requires = { { 'hoob3rt/lualine.nvim', opt=true }, {'kyazdani42/nvim-web-devicons', opt = true} }
+}
 
     use {
     'nvim-treesitter/nvim-treesitter',
