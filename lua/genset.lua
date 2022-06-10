0f015d169a798d219c4c94440277e089f6aafd2c
Gianni Young <GianniYoung@me.com>
2022-06-09 22:22:32 -0500

    <!doctype html> <html> <head> <meta charset="UTF-8"> <title>Commit Message Generator</title> <link rel="author" href="/humans.txt" /> <style> body { font-family: 'Lucida Console', 'Courier New', monospace; font-size: 26pt; line-height: 1.2em; } #content { width: 760px; text-align: left; margin: 1em auto; } .permalink { font-size: .5em; color: #ddd; line-height: 1em; } .permalink a { text-decoration: none; color: inherit; } .permalink a:hover { text-decoration: underline; } </style> </head> <body> <div id="content"> <p>Wubbalubbadubdub! </p> <p class="permalink"> [<a href="/9484125651f94fd7cb5dda8b2400de89">permalink</a>] </p> </div> </body> <!-- Like what you see? http://github.com/ngerakines/commitment --> </html>

--- lua/genset.lua
+++ lua/genset.lua
@@ -2,34 +2,34 @@
 -- General Settings
 ---
 HOME = os.getenv("HOME")
-vim.opt.clipboard=vim.opt.clipboard + 'unnamedplus'
-vim.opt.wildmode={longest='full','full'}
-vim.opt.expandtab=true
-vim.opt.relativenumber=true
-vim.opt.shiftwidth=4
-vim.opt.tabstop=2
-vim.opt.hidden=true
-vim.o.signcolumn='yes:2'
+vim.opt.clipboard = vim.opt.clipboard + 'unnamedplus'
+vim.opt.wildmode = { longest = 'full', 'full' }
+vim.opt.expandtab = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.hidden = true
+vim.o.signcolumn = 'yes:2'
 vim.o.listchars = 'tab:▸ ,trail:·'
-vim.opt.splitbelow=true
-vim.opt.splitright=true
-vim.opt.number=true
-vim.opt.termguicolors=true
-vim.opt.ignorecase=true
-vim.opt.ic=true
-vim.opt.smartcase=true
-vim.opt.wrap=false
-vim.opt.list=true
-vim.opt.mouse='a'
-vim.opt.scrolloff=3
-vim.opt.sidescrolloff=3
-vim.opt.joinspaces=false
-vim.opt.exrc=true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.ignorecase = true
+vim.opt.ic = true
+vim.opt.smartcase = true
+vim.opt.wrap = false
+vim.opt.list = true
+vim.opt.mouse = 'a'
+vim.opt.scrolloff = 3
+vim.opt.sidescrolloff = 3
+vim.opt.joinspaces = false
+vim.opt.exrc = true
 vim.o.updatetime = 150
-vim.opt.updatetime=300 -- Reduce time for highlighting other references
-vim.opt.redrawtime=13000 -- Allow more time for loading syntax on large files
-vim.opt.showmatch=true
-vim.opt.showmode=false
+vim.opt.updatetime = 300 -- Reduce time for highlighting other references
+vim.opt.redrawtime = 13000 -- Allow more time for loading syntax on large files
+vim.opt.showmatch = true
+vim.opt.showmode = false
 
 -- " set backup
 -- " set backupdir=~/.local/share/nvim/backup//
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,72 +10,70 @@ end
 vim.g.mapleader = ' '
 
 -- resize splits
-map('n','<M-j>',':resize -2<CR>')
-map('n','<M-k>',':resize +2<CR>')
-map('n','<M-h>',':vertical resize -2<CR>')
-map('n','<M-l>',':vertical resize +2<CR>')
+map('n', '<M-j>', ':resize -2<CR>')
+map('n', '<M-k>', ':resize +2<CR>')
+map('n', '<M-h>', ':vertical resize -2<CR>')
+map('n', '<M-l>', ':vertical resize +2<CR>')
 
 -- navigate splits
-map('n','<C-h>','<C-w>h')
-map('n','<C-j>','<C-w>j')
-map('n','<C-k>','<C-w>k')
-map('n','<C-l>','<C-w>l')
-map ('n','<leader>Q',':bufdo bdelete<CR>')
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+map('n', '<leader>Q', ':bufdo bdelete<CR>')
 
 -- navigate buffers
-map ('n','gk',':bn<CR>')
-map ('n','gj',':bp<CR>')
-map ('n','<leader>k',':bn<CR>')
-map ('n','<leader>j',':bp<CR>')
-map ('n','<leader>K',':tabn<CR>')
-map ('n','<leader>J',':tabp<CR>')
+map('n', 'gk', ':bn<CR>')
+map('n', 'gj', ':bp<CR>')
+map('n', '<leader>k', ':bn<CR>')
+map('n', '<leader>j', ':bp<CR>')
+map('n', '<leader>K', ':tabn<CR>')
+map('n', '<leader>J', ':tabp<CR>')
 
 -- file hopping
-map ('n','<leader>ve',':edit ~/.config/nvim/init.vim<cr>')
-map ('n','<leader>vk',':edit ~/.config/nvim/lua/keymaps.lua<cr>')
-map ('n','<leader>vp',':edit ~/.config/nvim/lua/plugins.lua<cr>')
-map ('n','<leader>vr',':source $MYVIMRC<cr>')
+map('n', '<leader>ve', ':edit ~/.config/nvim/init.vim<cr>')
+map('n', '<leader>vk', ':edit ~/.config/nvim/lua/keymaps.lua<cr>')
+map('n', '<leader>vp', ':edit ~/.config/nvim/lua/plugins.lua<cr>')
+map('n', '<leader>vr', ':source $MYVIMRC<cr>')
 
 -- saving
-map ('n','<leader>s',':w<CR>')
-map ('n','<leader>S',':wa<CR>')
+map('n', '<leader>s', ':w<CR>')
+map('n', '<leader>S', ':wa<CR>')
 
 -- misc
-map ('n','<leader>H',':nohlsearch<CR>')
-map ('n','<leader>c',':set cuc!<CR>')
-map ('n','gf',':edit <cfile><cr>')
+map('n', '<leader>H', ':nohlsearch<CR>')
+map('n', '<leader>c', ':set cuc!<CR>')
+map('n', 'gf', ':edit <cfile><cr>')
 
 -- Make Y behave like the other capitals
-map ('n','Y','y$')
+map('n', 'Y', 'y$')
 
 
 -- Reselect visual selection after indenting
-map ('v','<','<gv')
-map ('v','>','>gv')
+map('v', '<', '<gv')
+map('v', '>', '>gv')
 
 -- Maintain the cursor position when yanking a visual selection
 -- http://ddrscott.github.io/blog/2016/yank-without-jank/
-map ('v','y','myy`y')
-map ('v','Y','myY`y')
+map('v', 'y', 'myy`y')
+map('v', 'Y', 'myY`y')
 
 -- Paste replace visual selection without copying it
-map ('v','<leader>p','"_dP')
+map('v', '<leader>p', '"_dP')
 
 -- open link with default program
-map ('n','<leader>x',':!xdg-open %<cr><cr>')
+map('n', '<leader>x', ':!xdg-open %<cr><cr>')
 -- nmap <leader>x :!xdg-open %<cr><cr>
 
 -- Quicky escape to normal mode
-map ('i','ii','<esc>')
-map ('i',';;','<Esc>A;<Esc>')
-map ('i',',,','<Esc>A,<Esc>')
+map('i', 'ii', '<esc>')
+map('i', ';;', '<Esc>A;<Esc>')
+map('i', ',,', '<Esc>A,<Esc>')
 
-map ('c','w!!','%!sudo tee > /dev/null %')
+map('c', 'w!!', '%!sudo tee > /dev/null %')
 
-map('n','<leader>q',':Sayonara<CR>')
+map('n', '<leader>q', ':Sayonara<CR>')
 
 -- Ranger
-map('n','<leader>n',':RnvimrToggle<CR>')
-map('t','<leader>n','<C-\\><C-n>:RnvimrToggle<CR>')
-
-
+map('n', '<leader>n', ':RnvimrToggle<CR>')
+map('t', '<leader>n', '<C-\\><C-n>:RnvimrToggle<CR>')
