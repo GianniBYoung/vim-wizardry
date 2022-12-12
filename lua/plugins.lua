14fc7ca2a67ef3c3a80bbc1882bf7315779b7513
Young, Gianni <gianni.young@nelnet.net>
2022-12-12 12:12:32 -0600

    ensure treesitter parsers are installed

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -243,13 +243,7 @@ map('n', '<Leader>kk', ':ColorizerToggle<CR>')
 -- Tree Sitter
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "python", "ruby", "yaml", "go" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+  ensure_installed = { "python", "ruby", "yaml", "go", "bash", "dockerfile", "hcl", "json"  },
 
   highlight = {
     enable = true,
