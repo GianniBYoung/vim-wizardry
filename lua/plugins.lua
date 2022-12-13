ec4af778b913001e564b39b3ff3bac106c2069e9
Young, Gianni <gianni.young@nelnet.net>
2022-12-13 16:07:46 -0600

    tabline stuf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -205,8 +205,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
     disabled_filetypes = {},
     always_divide_middle = true,
     globalstatus = false,
@@ -214,7 +214,7 @@ require('lualine').setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { {'filename', path=1} },
+    lualine_c = { {'filename', path=0} },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
