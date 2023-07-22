2bc87393412f243424f35a640da443146057554a
Gianni Young <gianniyoung@protonmail.com>
2023-07-22 00:02:35 -0500

    fixed a few things

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,21 +31,21 @@ wk.register({
     j = { "<cmd>bp<cr>", "Previous Buffer" },
     k = { "<cmd>bn<cr>", "Next Buffer" },
 },
-["d"] = { "<cmd>TroubleToggle<cr>", "Trouble" },
 ["<leader>d"] = { name = "Trouble",
 d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Diagnostics" },
 r = { "<cmd>TroubleToggle lsp_references<cr>", "Lsp References" },
 f = { "<cmd>TroubleToggle quickfix<cr>", "Quick Fix" },
+j = { "<cmd>TroubleToggle<cr>", "Trouble"},
   },
   -- Splits find better binds
-  ["<M-J>"] = { "<cmd>resize -2<cr>", "Resize Splits"},
-  ["<M-K>"] = { "<cmd>resize +2<cr>"},
-  ["<M-H>"] = { "<cmd>vertical resize +2<cr>"},
-  ["<M-L>"] = {"<cmd>vertical resize +2<cr>" },
-  -- ["<M-j>"] = { "<C-w>j"},
-  -- ["<M-k>"] = { "<C-w>k"},
-  ["<M-h>"] = { "<C-w>h"},
-  ["<M-l>"] = { "<C-w>l", "Navigate Split" },
+  ["<C-J>"] = { "<cmd>resize -2<cr>", "Resize Splits"},
+  ["<C-K>"] = { "<cmd>resize +2<cr>"},
+  ["<C-H>"] = { "<cmd>vertical resize +2<cr>"},
+  ["<C-L>"] = {"<cmd>vertical resize +2<cr>" },
+  ["<C-j>"] = { "<C-w>j"},
+  ["<C-k>"] = { "<C-w>k"},
+  ["<C-h>"] = { "<C-w>h"},
+  ["<C-l>"] = { "<C-w>l", "Navigate Split" },
   ["<leader>"] = {name = "Vim Wiki",
   ww = { "<cmd>VimwikiIndex<cr>", "Vim Wiki" },
   lc = { "<cmd>set cursorbind<cr>", "Vim Wiki" },
@@ -60,11 +60,11 @@ f = { "<cmd>TroubleToggle quickfix<cr>", "Quick Fix" },
   p = { 'ysiw(', "Surround in Parens" },
   },
   ["<leader>v"] = {name = "File Hopping",
-  k = { "<cmd>e ~/.config/nvim/keymaps.lua<cr>", "Edit Vim Keybinds" },
-  p = { "<cmd>e ~/.config/nvim/plugins.lua<cr>", "Edit Vim Plugins" },
-  e = { "<cmd>e ~/.config/nvim/keymaps.lua<cr>", "Edit Vim Plugins" },
+  k = { "<cmd>e ~/.config/nvim/lua/keymaps.lua<cr>", "Edit Vim Keybinds" },
+  p = { "<cmd>e ~/.config/nvim/lua/plugins.lua<cr>", "Edit Vim Plugins" },
+  e = { "<cmd>e ~/.config/nvim/lua/keymaps.lua<cr>", "Edit Vim Plugins" },
   z = { "<cmd>e ~/.zshrc<cr>", "Edit Zshrc" },
-  a = { "<cmd>e ~/.config/nvim/autocmd.lua<cr>", "Edit Autocmds" },
+  a = { "<cmd>e ~/.config/nvim/lua/autocmd.lua<cr>", "Edit Autocmds" },
   r = { "<cmd>luafile %<cr>", "Reload Vim Confs" },
   },
   ["<leader>cd"] = { "<cmd>cd %:h<cr>", "Cd" },
@@ -83,21 +83,22 @@ f = { "<cmd>TroubleToggle quickfix<cr>", "Quick Fix" },
   ["<M-j>"] = { "<cmd>call smooth_scroll#down(10, 5, 2)cr>", "Smooth Scroll"},
   ["Y"] = { "y$", "Papa Yank"},
   ["<leader>n"] = { "<cmd>cd %:h<bar>RnvimrToggle<cr>", "Ranger" },
-  ["<leader>n"] = { "<C-\\><C-n>:RnvimrToggle<CR>", "Ranger", mode = "t" },
   ["<leader>gr"] = { "<cmd>w<cr><cmd>GoRun<cr>", "Go Run" },
   ["ii"] = { "<esc>", "which_key_ignore", mode = "i"},
   ["<F1>"]= { "<cmd>FloatermToggle<cr>", "Terminal" },
+  })
+
+  wk.register({
   ["<F1>"]= { "<C-\\><C-n>::FloatermToggle<CR>", "Terminal", mode = "t" },
+  ["<leader>n"] = { "<C-\\><C-n>:RnvimrToggle<CR>", "Ranger", mode = "t" },
+  })
+
+
+  wk.register({
+  ["Y"]= { "myY`y", mode = "v", },
+  ["<leader>r"]= { "<cmd>Hypersonic<cr>", mode = "v" },
   ["<"]= { "<gv", "Indent", mode = "v" },
   [">"]= { ">gv", "Dedent", mode = "v" },
   ["y"]= { "myy`y", mode = "v" },
-  ["Y"]= { "myY`y", mode = "v" },
   ["<leader>p"]= { '"_dP', mode = "v" },
-  ["<leader>r"]= { "<cmd>Hypersonic<cr>", mode = "v" },
-
   })
-
-  -- |g:AutoPairsMapBS|                                                          int
-  -- Default: 1
-  -- Map <BS> to delete brackets and quotes in pair, executes:
-  --     inoremap <buffer> <silent> <BS> <C-R>=AutoPairsDelete()<CR>
