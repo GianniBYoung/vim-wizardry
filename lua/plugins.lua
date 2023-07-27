d60d12ec9d542105cb5cea646514db398d7ed8e7
Young, Gianni <gianni.young@nelnet.net>
2023-07-27 08:06:57 -0500

    formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -258,7 +258,7 @@ local lsp_attach = function(_, bufnr)
         ["<leader>ca"] ={vim.lsp.buf.code_action, "Code Action", bufopts},
         ["<leader>rn"] ={vim.lsp.buf.rename, "Refactor Name", bufopts},
         -- ["<space>p"] ={vim.lsp.buf.format{async = true}, "Format", bufopts},
-        ["<space>r"] ={vim.lsp.buf.format, "Format", bufopts},
+        ["<leader>lr"] ={vim.lsp.buf.format, "Format", bufopts},
     })
 end
 
@@ -364,10 +364,12 @@ require("noice").setup({
             ["cmp.entry.get_documentation"] = true,
         },
     },
+    -- These are not actually doing anything right now
     presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
+        long_message_to_split = false, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
     },
