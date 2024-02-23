3da6c5ea83d9f4c8a4d6e7797b18b5690aea35eb
Gianni Young <gianniyoung@protonmail.com>
2024-02-22 22:10:07 -0600

    fr

--- lua/plugins/plugs.lua
+++ lua/plugins/plugs.lua
@@ -5,7 +5,6 @@ return {
     {'tpope/vim-eunuch', event = "VeryLazy"},
     {'TobinPalmer/pastify.nvim', cmd = { 'Pastify' }},
     {'voldikss/vim-floaterm',lazy = true, cmd = 'FloatermToggle' },
-    -- {'machakann/vim-highlightedyank'}, autocommand for this
     {'lervag/wiki.vim', lazy = true, cmd = 'WikiIndex'},
     {'0x00-ketsu/markdown-preview.nvim', ft = { 'md', 'markdown', 'mkd', 'mkdn', 'mdwn', 'mdown', 'mdtxt', 'mdtext', 'rmd', 'wiki' } },
     {'lewis6991/gitsigns.nvim',
