3a0ccb5359d95f2c012a732df1f6c567cad28041
Young, Gianni <gianni.young@nelnet.net>
2024-02-23 15:21:35 -0600

    fixing pastify

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,6 +3,6 @@ return {
         '0x00-ketsu/markdown-preview.nvim',
         ft = { 'md', 'markdown', 'mkd', 'mkdn', 'mdwn', 'mdown', 'mdtxt', 'mdtext', 'rmd', 'wiki' }
     },
-    { 'TobinPalmer/pastify.nvim', cmd = { 'Pastify' } },
+    { 'TobinPalmer/pastify.nvim', cmd = { 'Pastify' }, config = true },
     { 'lervag/wiki.vim',          lazy = true,        cmd = 'WikiIndex' },
 }
