69f5cb8e1f4992009b659e1e4f3a0d3f9a2bd88b
Young, Gianni <gianni.young@nelnet.net>
2024-03-06 16:55:55 -0600

    map cleaning

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -23,22 +23,27 @@ vim.keymap.set("n", "<leader>bn", "<cmd>DapStepOver<cr>", { desc = "Step  Over" 
 vim.keymap.set("n", "<leader>bi", "<cmd>DapStepInto<cr>", { desc = "Step  Into" })
 vim.keymap.set("n", "<leader>bo", "<cmd>DapStepOut<cr>", { desc = "Step  Out" })
 vim.keymap.set("n", "<leader>br", "<cmd>DapToggleRepl<cr>", { desc = "Repl" })
-vim.keymap.set("n", "<leader>be", "<cmd>lua require'dapui'.eval()<cr>", { desc = "Evaluate  Under Cursor" })
-vim.keymap.set("n", "<leader>bt", "<cmd>lua require'dapui'.toggle()<cr>", { desc = "Toggle  DAP-UI" })
+
+vim.keymap.set("n", "<leader>be", function()
+	require("dapui").eval()
+end, { desc = "Evaluate  Under Cursor" })
+
+-- vim.keymap.set("n", "<leader>be", "<cmd>lua require'dapui'.eval()<cr>", { desc = "Evaluate  Under Cursor" })
+-- vim.keymap.set("n", "<leader>bt", "<cmd>lua require'dapui'.toggle()<cr>", { desc = "Toggle  DAP-UI" })
+vim.keymap.set("n", "<leader>bt", function()
+	require("dapui").toggle()
+end, { desc = "Toggle  DAP-UI" })
 
 wk.register({
 	-- Individuals
 	[","] = { "<cmd>QuickBufferJump<cr>", "Buffer Jump" },
 	-- ["<C-n>"] = { "<cmd>Telescope fd prompt_prefix=🔍<cr>", "Find Files" },
-	["<C-b>"] = { "<cmd>Telescope buffers prompt_prefix=📜<cr>", "Find Open Buffers" },
 	["<leader>r"] = { "<cmd>Telescope registers prompt_prefix=📠<cr>", "Paste From Register" },
-	["<C-t>"] = { "<cmd>Telescope resume<cr>", "Resume Last Telscope" },
 	["<C-h>"] = { "<C-w>h", "Select 'Left' Window" },
 	["<C-j>"] = { "<C-w>j", "Select 'Lower' Window" },
 	["<C-k>"] = { "<C-w>k", "Select 'Upper' Window", noremap = true },
 	["<C-l>"] = { "<C-w>l", "Select 'Right' Window" },
 	["g"] = {
-		t = { "<cmd>Telescope lsp_type_definitions<cr>", "Lsp Type Defs" },
 		j = { "<cmd>bp<cr>", "Previous Buffer" },
 		k = { "<cmd>bn<cr>", "Next Buffer" },
 	},
@@ -71,21 +76,18 @@ wk.register({
 	["<leader>s"] = { "<cmd>w<cr>", "Save Buffer" },
 	["<leader>S"] = { "<cmd>wa<cr>", "Save All Buffers" },
 	["<leader>kk"] = { "<cmd>ColorizerToggle<cr>", "Colorizer" },
-	["<leader>H"] = { "<cmd>nohlsearch<cr>", "Clear Highlights" },
 	["<leader>C"] = { "<cmd>set cuc!<cr>", "Cursor Column" },
 	["<leader>R"] = { "<cmd>set relativenumber!<cr><cmd>set cursorline!<cr>", "Screen Sharing Mode" },
 	["gf"] = { "<cmd>edit <cfile><cr>", "Edit File Under Cursor" },
 	["<cr>"] = { "ciw", "Change Word Under Cursor" },
 	["<leader>m"] = { "<cmd>MPToggle<cr>", "Open with Glow" },
-	["<leader>x"] = { "<cmd>!xdg-open %<cr><cr>", "Open" },
 	["<leader>q"] = { "<cmd>lua MiniBufremove.delete(0,true)<cr>", "Close Buffer" },
 	["<leader>Q"] = { "<cmd>qa!<cr>", "The Nuclear Option" },
 	["<M-d>"] = { "<cmd>call smooth_scroll#up(10, 5, 2)<cr>", "Smooth Scroll" },
 	["<M-f>"] = { "<cmd>call smooth_scroll#down(10, 5, 2)<cr>", "Smooth Scroll" },
 	["Y"] = { "y$", "Papa Yank" },
 	["<leader>N"] = { '<cmd>lua require("ranger-nvim").open(true)<cr>', "Ranger" },
-	["<leader>n"] = { "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>", "Ranger" },
-	["<leader>gr"] = { "<cmd>w<cr><cmd>GoRun<cr>", "Go Run" },
+	["<leader>n"] = { "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>", "MiniFiles" },
 	["ii"] = { "<esc>", "which_key_ignore", mode = "i" },
 	["<F1>"] = { "<cmd>FloatermToggle<cr>", "Terminal" },
 })
