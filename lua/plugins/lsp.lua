7e1da7f3dcc281d1bc1dd8d728e372bfe6cb8cfd
Gianni Young <gianniyoung@protonmail.com>
2024-03-20 21:34:36 -0500

    rounded lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -233,20 +233,20 @@ return {
 						return item
 					end,
 				},
-				-- window = {
-				-- 	completion = {
-				-- 		border = "rounded",
-				-- 		winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None",
-				-- 		col_offset = -3,
-				-- 		side_padding = 1,
-				-- 		scrollbar = false,
-				-- 		scrolloff = 8,
-				-- 	},
-				-- 	documentation = {
-				-- 		border = "rounded",
-				-- 		winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
-				-- 	},
-				-- },
+				window = {
+					completion = {
+						border = "rounded",
+						winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None",
+						col_offset = -3,
+						side_padding = 1,
+						scrollbar = false,
+						scrolloff = 8,
+					},
+					documentation = {
+						border = "rounded",
+						winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
+					},
+				},
 				sources = {
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
