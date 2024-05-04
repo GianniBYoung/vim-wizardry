89178287ef31c2980a52f0efe72b9e0e2e40e7d8
Gianni Young <cmancy@tutanota.com>
2024-05-04 10:49:20 -0500

    removing dead code
    d

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -4,37 +4,10 @@ vim.api.nvim_create_autocmd(
 	{ pattern = { "*.txt", "*.md", "*.tex", "*.wiki" }, command = "setlocal spell" }
 )
 
--- -- -- -- Set spellcheck on markdown and text
--- -- -- vim.api.nvim_create_autocmd(
--- -- --     { "BufRead" },
--- -- --     { pattern = { "*.tfvars" }, command = "set ft=tf" }
--- -- -- )
--- -- --
--- -- -- from terrafor-ls docs
--- -- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
--- --     pattern = { "*.tf", "*.tfvars" },
--- --     callback = function()
--- --         vim.lsp.buf.format()
--- --     end,
--- -- })
--- --
--- -- This is not being set by the lsp, I need to set the option below somehow (add a condition to the mini config function)
--- vim.api.nvim_create_autocmd(
---     { "BufRead" },
---     { pattern = "*tfvars", command = "lua vim.bo.commentstring='# %s'" }
--- )
---
 vim.api.nvim_create_autocmd(
 	"BufWritePre",
 	{ desc = "strip trailing spaces on save", pattern = "*", command = "%s/\\s\\+$//e" }
-	-- { pattern = "\\\.(?!md).*", command = "%s/\\s\\+$//e" }
 )
-
--- vim.api.nvim_create_autocmd(
---     "BufWritePre",
---     { pattern = "*.go", command = 'lua vim.lsp.buf.format()' }
--- )
---
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
