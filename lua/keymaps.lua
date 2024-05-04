b3a2f4045f2f047f357b0f2a7b096199c6ee709f
Gianni Young <cmancy@tutanota.com>
2024-05-04 11:34:47 -0500

    even more

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,6 +15,7 @@ vim.keymap.set("n", "<leader>sf", "<cmd>Telescope fd<cr>", { desc = "Find Files"
 vim.keymap.set("n", "<leader>sz", "<cmd>Telescope spell_suggest<cr>", { desc = "Spelling Corrections" })
 vim.keymap.set("n", "<leader>sb", "<cmd>BookmarksGoto<cr>", { desc = "List Bookmarks" })
 vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep prompt_prefix=🔦<cr>", { desc = "grep" })
+vim.keymap.set("n", "<leader>r", "<cmd>Telescope registers prompt_prefix=📠<cr>", { desc = "Paste From Register" })
 
 --DAP
 vim.keymap.set("n", "<leader>bc", "<cmd>DapContinue<cr>", { desc = "Continue" })
@@ -26,29 +27,6 @@ vim.keymap.set("n", "<leader>br", "<cmd>DapToggleRepl<cr>", { desc = "Repl" })
 vim.keymap.set("n", "<C-g>", "<cmd>cprev<cr>", { desc = "Prev quick fix" })
 vim.keymap.set("n", "<C-;>", "<cmd>cnext<cr>", { desc = "Next quick fix" })
 
---Misc
-vim.keymap.set("n", "<leader>m", "<cmd>MPToggle<cr>", { desc = "Open with Glow" })
-vim.keymap.set("n", "Y", "y$", { desc = "Papa Yank" })
-vim.keymap.set("n", "<leader>N", '<cmd>lua require("ranger-nvim").open(true)<cr>', { desc = "Ranger" })
-vim.keymap.set("n", "<leader>n", "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>", { desc = "MiniFiles" })
-vim.keymap.set("n", "<F1>", "<cmd>FloatermToggle<cr>", { desc = "Terminal" })
-
-vim.keymap.set("n", "<leader>cd", "<cmd>cd %:h<cr>", { desc = "Cd" })
-vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save Buffer" })
-vim.keymap.set("n", "<leader>W", "<cmd>wa<cr>", { desc = "Save All Buffers" })
-vim.keymap.set("n", "<leader>kk", "<cmd>ColorizerToggle<cr>", { desc = "Colorizer" })
-vim.keymap.set("n", "<leader>C", "<cmd>set cuc!<cr>", { desc = "Cursor Column" })
-vim.keymap.set(
-	"n",
-	"<leader>R",
-	"<cmd>set relativenumber!<cr><cmd>set cursorline!<cr>",
-	{ desc = "Screen Sharing Mode For Normies" }
-)
-vim.keymap.set("n", "gf", "<cmd>edit <cfile><cr>", { desc = "Edit File Under Cursor" })
-vim.keymap.set("n", "<cr>", "ciw", { desc = "Change Word Under Cursor" })
-vim.keymap.set("n", "<leader>q", "<cmd>lua MiniBufremove.delete(0,true)<cr>", { desc = "Close Buffer" })
-vim.keymap.set("n", "<leader>Q", "<cmd>qa!<cr>", { desc = "The Nuclear Option" })
-
 vim.keymap.set("n", "<leader>be", function()
 	require("dapui").eval()
 end, { desc = "Evaluate  Under Cursor" })
@@ -59,50 +37,56 @@ vim.keymap.set("n", "<leader>bt", function()
 	require("dapui").toggle()
 end, { desc = "Toggle  DAP-UI" })
 
-wk.register({
-	-- Individuals
-	-- [","] = { "<cmd>QuickBufferJump<cr>", "Buffer Jump" },
-	-- ["<C-n>"] = { "<cmd>Telescope fd prompt_prefix=🔍<cr>", "Find Files" },
-	["<leader>r"] = { "<cmd>Telescope registers prompt_prefix=📠<cr>", "Paste From Register" },
-	["<C-h>"] = { "<C-w>h", "Select 'Left' Window" },
-	["<C-j>"] = { "<C-w>j", "Select 'Lower' Window" },
-	["<C-k>"] = { "<C-w>k", "Select 'Upper' Window", noremap = true },
-	["<C-l>"] = { "<C-w>l", "Select 'Right' Window" },
-	["g"] = {
-		j = { "<cmd>bp<cr>", "Previous Buffer" },
-		k = { "<cmd>bn<cr>", "Next Buffer" },
-	},
-	["<leader>p"] = {
-		name = "Projects",
-		p = { "<cmd>Telescope neovim-project discover<cr>", "Projects" },
-		o = { "<cmd>Telescope neovim-project history<cr>", "Open Project History" },
-		l = { "<cmd>NeovimProjectLoadRecent<cr>", "Open Last Project" },
-	},
-	["<leader>w"] = {
-		name = "Wiki",
-		w = { "<cmd>WikiIndex<cr>", "Wiki Index" },
-		o = { "<cmd>WikiOpen<cr>", "Wiki Open" },
-		e = { "<cmd>WikiExport -output ./pdfs<cr>", "Export Wiki Page to PDF" },
-	},
-	["<M-Enter>"] = { "<cmd>WikiLinkFollow<cr>", "Follow link" },
-	["<leader>l"] = {
-		name = "LazyGit",
-		g = { "<cmd>:LazyGitCurrentFile<cr>", "LazyGit" },
-		l = { "<cmd>Lazy<cr>", "Lazy" },
-		m = { "<cmd>Mason<cr>", "Mason" },
-		c = { "<cmd>set cursorbind!<cr>", "Cursor Bind" },
-		h = { "<cmd>set cursorline!<cr>", "Cursor Line" },
-		R = { "<cmd>lua vim.lsp.buf.format()<cr>", "lsp format that shouldn't be here" },
-	},
-	["<leader>v"] = {
-		r = { "<cmd>luafile %<cr>", "Reload a File" },
-	},
-})
+--Misc
+vim.keymap.set("n", "<F1>", "<cmd>FloatermToggle<cr>", { desc = "Terminal" })
+vim.keymap.set("t", "<F1>", "<C-\\><C-n>::FloatermToggle<CR>", { desc = "Toggle Term" })
+vim.keymap.set("n", "<leader>m", "<cmd>MPToggle<cr>", { desc = "Open with Glow" })
+vim.keymap.set("n", "<leader>kk", "<cmd>ColorizerToggle<cr>", { desc = "Colorizer" })
 
-wk.register({
-	["<F1>"] = { "<C-\\><C-n>::FloatermToggle<CR>", "Terminal", mode = "t" },
-	-- ["<leader>n"] = { "<C-\\><C-ESC>:RnvimrToggle<CR>", "Exit Term Mode", mode = "t" },
-})
+vim.keymap.set("n", "<leader>C", "<cmd>set cuc!<cr>", { desc = "Cursor Column" })
+vim.keymap.set(
+	"n",
+	"<leader>R",
+	"<cmd>set relativenumber!<cr><cmd>set cursorline!<cr>",
+	{ desc = "Screen Sharing Mode For Normies" }
+)
+vim.keymap.set("n", "<leader>vr", "<cmd>luafile %<cr>", { desc = "Reload a File" })
+
+-- enhanced motions
+vim.keymap.set("n", "Y", "y$", { desc = "Papa Yank" })
+vim.keymap.set("n", "<cr>", "ciw", { desc = "Change Word Under Cursor" })
+vim.keymap.set("n", "<leader>cd", "<cmd>cd %:h<cr>", { desc = "Cd" })
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save Buffer" })
+vim.keymap.set("n", "<leader>W", "<cmd>wa<cr>", { desc = "Save All Buffers" })
+
+-- nav
+vim.keymap.set("n", "<leader>n", "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>", { desc = "MiniFiles" })
+vim.keymap.set("n", "gf", "<cmd>edit <cfile><cr>", { desc = "Edit File Under Cursor" })
+vim.keymap.set("n", "<leader>N", '<cmd>lua require("ranger-nvim").open(true)<cr>', { desc = "Ranger" })
+vim.keymap.set("n", "<leader>q", "<cmd>lua MiniBufremove.delete(0,true)<cr>", { desc = "Close Buffer" })
+vim.keymap.set("n", "<leader>Q", "<cmd>qa!<cr>", { desc = "The Nuclear Option" })
+vim.keymap.set("n", "<C-w>h", "", { desc = "Select 'Left' Window" })
+vim.keymap.set("n", "<C-w>j", "", { desc = "Select 'Lower' Window" })
+vim.keymap.set("n", "<C-w>k", "", { desc = "Select 'Upper' Window", noremap = true })
+vim.keymap.set("n", "<C-w>l", "", { desc = "Select 'Right' Window" })
+vim.keymap.set("n", "gj", "<cmd>bp<cr>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "gk", "<cmd>bn<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "pp", "<cmd>Telescope neovim-project discover<cr>", { desc = "Projects" })
+vim.keymap.set("n", "po", "<cmd>Telescope neovim-project history<cr>", { desc = "Open Project History" })
+vim.keymap.set("n", "pl", "<cmd>NeovimProjectLoadRecent<cr>", { desc = "Open Last Project" })
+
+-- wiki
+vim.keymap.set("n", "ww", "<cmd>WikiIndex<cr>", { desc = "Wiki Index" })
+vim.keymap.set("n", "wo", "<cmd>WikiOpen<cr>", { desc = "Wiki Open" })
+vim.keymap.set("n", "we", "<cmd>WikiExport -output ./pdfs<cr>", { desc = "Export Wiki Page to PDF" })
+vim.keymap.set("n", "<M-Enter>", "<cmd>WikiLinkFollow<cr>", { desc = "Follow Wiki Link" })
+
+-- Git (More to Come)
+vim.keymap.set("n", "<leader>lg", "<cmd>:LazyGitCurrentFile<cr>", { desc = "" })
+
+-- Plugin Management
+vim.keymap.set("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
+vim.keymap.set("n", "<leader>lm", "<cmd>Mason<cr>", { desc = "Mason" })
 
 wk.register({
 	["Y"] = { "myY`y", mode = "v" },
